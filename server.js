7d260b92b19f31586abbedb429e719b206b643f8
amanj <amanj@pop-os.localdomain>
2022-07-18 18:00:37 +0430

    oauth2 added

--- server.js
+++ server.js
@@ -2,15 +2,32 @@ const fs = require("fs");
 const path = require("path");
 const https = require("https");
 const http = require("http");
+const helmet = require("helmet");
 const express = require("express");
 
 const app = express();
+
+app.use(helmet());
+
+const checkedLoggedIn = (req, res, next) => {
+  const isLoggedIn = true; //TODO
+  if (!isLoggedIn) {
+    res.status(401).json({ error: "you must login !" });
+  }
+  next();
+};
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "pulic", "index.html"));
 });
 
-app.get("/secret", (req, res) => {
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkedLoggedIn, (req, res) => {
   res.send("this is your secret 23 :)");
 });
 
